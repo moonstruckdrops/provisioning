c18872a0c8d249e7147e53796769653169f9a3c4
moonstruckdrops <moonstruckdrops@gmail.com>
2017-01-01 15:17:09 +0900

    OSX setup

---
+++ osx.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+declare -r XCODE_URL="https://itunes.apple.com/jp/app/id497799835"
+
+function install_xcode() {
+    # xcodeのバージョンチェック
+    xcodebuild -version > /dev/null 2>&1
+
+    if [ $? -eq 0 ]; then
+        notify_osx "Xcode is installed."
+    else
+        notify_osx "Xcode is not intalled."
+        notify_osx "See Xcode at $XCODE_URL"
+        # xcodeのcommand line toolsのインストール
+        xcode-select --install
+        softwareupdate -i CommandLineTools
+    fi
+}
+
+function install_homebrew() {
+    notify_osx "install homebrew"
+    which brew > /dev/null 2>&1
+    if [ "$?" -ne 0 ]; then
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+    brew update
+}
+
+function notify_osx() {
+    osascript -e "display notification \"${1}\" with title \"osx-provisioing\""
+    say ${1}
+}
---
+++ setup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash -e
+
+. $(dirname $0)/osx.sh
+
+function install_ansible() {
+    echo "install_ansible"
+
+    if [ `echo "${OSTYPE}" |grep "linux*"` ]; then
+        which pip >/dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            echo "not found command pip"
+            echo "install pip"
+            sudo easy_install pip
+        fi
+
+        which ansible >/dev/null 2>&1
+        if [ $? -ne 0 ]; then
+            echo "not found command ansible"
+            echo "install ansible"
+            sudo pip install ansible
+        fi
+    else
+        brew install python
+        brew install ansible
+    fi
+}
+
+function provisioning() {
+    if [ `echo "${OSTYPE}" |grep "linux*"` ]; then
+        setup_linux
+    else
+        ansible-playbook osx.yml --connection=local --vault-password-file .vault_pass --extra-vars=@vault.yml
+    fi
+}
+
+function setup_osx() {
+    notify_osx "start osx setup"
+    install_xcode
+    install_homebrew
+    install_ansible
+    provisioning
+    notify_osx "finish osx setup"
+}
+
+function setup_linux() {
+    # TODO: Linuxのセットアップで必須な内容はあればここに書く
+    echo "do nothing."
+    install_ansible
+    provisioning
+}
+
+function _main() {
+    if [ `echo "${OSTYPE}" |grep "linux*"` ]; then
+        setup_linux
+    else
+        setup_osx
+    fi
+}
+
+_main
